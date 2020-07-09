8b6f81eeab937e123e49993cbd12bfac138bc033
Emin <israfilzadehemin@gmail.com>
2020-07-09 22:38:57 +0400

    -- formLogin() FakeDB implemented

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,24 +1,19 @@
 package com.example.demo.security;
 
-import com.example.demo.student.Student;
+import com.example.demo.auth.ApplicationUserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.example.demo.security.ApplicationUserPermission.*;
 import static com.example.demo.security.ApplicationUserRole.*;
 
 @Configuration
@@ -26,9 +21,11 @@ import static com.example.demo.security.ApplicationUserRole.*;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
   private final PasswordEncoder passwordEncoder;
+  private final ApplicationUserService applicationUserService;
 
-  public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+  public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService) {
     this.passwordEncoder = passwordEncoder;
+    this.applicationUserService = applicationUserService;
   }
 
   @Override
@@ -64,35 +61,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
   }
 
   @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.authenticationProvider(daoAuthenticationProvider());
+  }
+
   @Bean
-  protected UserDetailsService userDetailsService() {
-    UserDetails annaSmithUser = User.builder()
-            .username("annasmith")
-            .password(passwordEncoder.encode("password"))
-//            .roles(STUDENT.name()) // ROLE_STUDENT
-            .authorities(STUDENT.getGrantedAuthorities())
-            .build();
-
-    UserDetails lindaUser = User.builder()
-            .username("linda")
-            .password(passwordEncoder.encode("password123"))
-//            .roles(ADMIN.name()) // ROLE_ADMIN
-            .authorities(ADMIN.getGrantedAuthorities())
-            .build();
-
-    UserDetails tomUser = User.builder()
-            .username("tom")
-            .password(passwordEncoder.encode("password123"))
-//            .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
-            .authorities(ADMINTRAINEE.getGrantedAuthorities())
-            .build();
-
-
-    return new InMemoryUserDetailsManager(
-            annaSmithUser,
-            lindaUser,
-            tomUser
-
-    );
+  public DaoAuthenticationProvider daoAuthenticationProvider() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setPasswordEncoder(passwordEncoder);
+    provider.setUserDetailsService(applicationUserService);
+    return provider;
   }
 }
